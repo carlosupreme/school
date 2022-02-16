6e590066079c61be646bb0ab49eaf4c4cf1f7e52
Carlos Sosa <carlos.alberto.sosa.perera@gmail.com>
2022-02-16 13:01:39 -0600

    termino el home page con sus estilos y sus archivos

--- js/components/Home.js
+++ js/components/Home.js
@@ -3,9 +3,12 @@ import createDiv from "../helpers/createDiv.js";
 export default function Home() {
   let username = localStorage.getItem("username")
   let $div = createDiv(`
-    <h1>Home page</h1>
-    <h2>Bienvenido ${username}</h2>
-    <p>En esta pagina encontraras los apuntes de la carrera Sistemas Computacionales y muchas mas cosas</p>
-  `);
+    <div>
+      <h2>Bienvenido a la pagina principal</h2>
+      <p>En esta pagina encontraras los apuntes de la carrera <strong>Sistemas Computacionales</strong> y muchas mas cosas...</p>
+    </div>
+    <div class="bg"></div>
+    `);
+  $div.classList = "home";
   return $div;
 }
